74e880f94f71bb3d8c52b979366d96cb3538a843
BlurplePanda <90356225+BlurplePanda@users.noreply.github.com>
2022-03-30 14:52:31 +1300

    New driver class with array
    Sets up flowers with array and for loops

---
+++ ClickGarden.java
@@ -0,0 +1,46 @@
+import ecs100.*;
+import java.awt.Color;
+
+/**
+ * Create a garden with each object in an array
+ * Click on the flower and make it grow
+ *
+ * @author Amy
+ * @version 30/03/22
+ */
+public class ClickGarden
+{
+    // make an array of flowers
+    private final int STARTX = 75;
+    private final int YPOS = 100;
+    private final int MAXFLOWERS = 5;
+    
+    private Flower[] flowerBed = new Flower[MAXFLOWERS]; // array of flowers
+    
+    /**
+     * Constructor for objects of class ClickGarden
+     */
+    public ClickGarden() {
+        UI.initialise();
+        // UI.addButton("Grow", this::growGarden);
+        UI.addButton("Random Grow", this::randomGrow);
+        UI.addButton("Quit", UI::quit);
+        
+        // set up the flower position
+        for (int i = 0; i < MAXFLOWERS; i++) {
+            // set every flower to a random colour
+            Color col = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+            flowerBed[i] = new Flower(STARTX*(i+1), YPOS, 10, 50, col); // create the flower object put in array
+        }
+        
+        // draw each flower
+        for (Flower flower : flowerBed) {
+            flower.draw();
+        }
+        
+        // set up mouse
+        UI.setMouseListener(this::doMouse);
+    }
+    
+    
+}
